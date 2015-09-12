8cb35110425f96c5f33e509c6226f2bcd0b5e2a1
maweiyi <ma823285807@gmail.com>
2015-09-12 18:57:22 +0800

    “Second commit”

--- Music/ViewController.swift
+++ Music/ViewController.swift
@@ -8,12 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     var viewAppr:UIView = UIView()
 
 
-    @IBOutlet weak var viewPosition: UIView!
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,6 +22,9 @@ class ViewController: UIViewController {
         /*self.viewAppr.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: 20);
         self.viewAppr.backgroundColor = UIColor(red: 233/255, green: 142/255, blue: 158/255, alpha: 1)
         self.view.addSubview(self.viewAppr)*/
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "Cell")
         
     }
 
@@ -28,6 +32,32 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell: UITableViewCell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as! UITableViewCell
+        //var layer: CALayer
+        cell.imageView?.image = UIImage(named: "headerImage")
+        cell.imageView?.layer.cornerRadius = 30
+        
+        print("\(cell.imageView?.image?.size.width)")
+        cell.imageView?.clipsToBounds = true
+        
+        cell.textLabel?.text = "maweiyi"
+        return cell as UITableViewCell
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 88.0
+    }
+    
 
 
 }
